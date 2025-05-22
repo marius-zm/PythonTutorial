8f4eba7d82b6c9be98bfda98738790fdd38efbbb
marius <marius@gmail.com>
2025-05-22 14:08:41 +0200

    Fertig

--- 3.1.4.py
+++ 3.1.4.py
@@ -1,3 +1,35 @@
+# 1/24
+
 # Dies ist ein Kommentar
 
 print("Hello World!")
+
+# 2/24
+
+print(2)
+print(-2)
+print(5 + 5 * 2 - 10)
+print(1.7 * 0.2 / 0.111)
+
+# 3/24
+
+print("Hello " + "World!" + " 2")
+print('Hallöchen')
+print("""Text über mehrere Zeilen
+      
+      fasdfj""")
+
+# 4/24
+
+name = "Marius"
+print(name)
+name = 2
+print(name + 2.5)
+
+# 5/24
+
+print("10" + "2")
+# print("Hallo" + 10)
+int_variable = 1700
+float_variable = 2.5
+string_variable = "String"
